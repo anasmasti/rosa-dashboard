900f72d769e06f77950933ae3b35b8817f12b2b0
anasmasti <anasmasti@hotmail.com>
2022-08-08 11:14:50 +0100

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className=''>
+        Rosa Dashboard
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
